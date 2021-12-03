1970e6378b24681fb3762492a8ce13c6869cd853
Peter Knoch <pwknoch@gmail.com>
2021-12-02 18:25:42 -0900

    Work on 2048 game

--- games/2048.js
+++ games/2048.js
@@ -273,3 +273,49 @@ function generateFullBadBoard() {
 
 var undoButton = document.querySelector('#undo-button');
 undoButton.addEventListener('click', undoMove);
+
+document.addEventListener('touchstart', handleTouchStart, false);
+document.addEventListener('touchmove', handleTouchMove, false);
+
+var xDown = null;
+var yDown = null;
+
+function getTouches(evt) {
+    return evt.touches ||             // browser API
+        evt.originalEvent.touches; // jQuery
+}
+
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];
+    xDown = firstTouch.clientX;
+    yDown = firstTouch.clientY;
+};
+
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
+
+    var xUp = evt.touches[0].clientX;
+    var yUp = evt.touches[0].clientY;
+
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+        if ( xDiff > 0 ) {
+            handleKey('ArrowRight');
+        } else {
+            handleKey('ArrowLeft');
+        }
+    } else {
+        if ( yDiff > 0 ) {
+            handleKey('ArrowDown');
+        } else {
+            handleKey('ArrowUp');
+        }
+    }
+    /* reset values */
+    xDown = null;
+    yDown = null;
+};
